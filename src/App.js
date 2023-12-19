acaec5d9942d2db32ff02e658c71ca652d9a06e6
kushagra331 <kushagrasharivastava331@gmail.com>
2023-12-19 23:42:36 +0530

    qrone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 function App() {
+  const [qrCode,SetQrCode]=useState('');
+  const [input,setInput]=useState('');
+  // function handleGenerateQrCode(){
+  //   SetQrCode(input);
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code generator</h1>
+      <div>
+        <input 
+        onChange={(e)=>{
+          setInput(e.target.value);
+          SetQrCode(input);
+        }}
+        type="text" name="qr-code" id="Enter your value" />
+        {/* <button
+        disabled={input && input.trim()!==''?false:true} 
+        onClick={()=>SetQrCode(input)}>Generate
+        </button> */}
+      </div>
+      <div>
+        <QRCode
+        id="qr-code-value"
+        value={qrCode}
+        size={400}
+        bgColor="#fff"
+        />
+      </div>
     </div>
   );
 }
